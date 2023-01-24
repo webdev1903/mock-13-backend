8206d6d33230cf6f4e624a1417d40b191b5d42b8
webdev1903 <vantureprakash1903@gmail.com>
2023-01-24 13:00:18 +0530

    change user model

--- models/user.model.js
+++ models/user.model.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
+    name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
   },
